6172283e58b7242d86b3dcf2e6f160d9402f6886
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2020-03-08 09:21:49 +0800

    fix eal_rte_init parameter convert error
    In previous version, the argv is not ended with "\0",
    so it may cause dpdk read a non exist hugepage dir

--- src/dpdk/mod.rs
+++ src/dpdk/mod.rs
@@ -1,3 +1,4 @@
+use std::ffi::CString;
 use std::os::raw::{c_char, c_int};
 
 use super::error::Error;
@@ -9,22 +10,29 @@ pub use raw::*;
 
 /// DPDK EAL initialization
 pub fn eal_init(dpdk_eal_args: &mut Vec<String>) -> Result<(), Error> {
-    let mut c_args = dpdk_eal_args
+    let mut c_args: Vec<*mut c_char> = dpdk_eal_args
         .iter_mut()
-        .map(|arg| arg.as_mut_ptr() as *mut i8)
-        .collect::<Vec<*mut c_char>>();
+        .map(|arg| CString::new(arg.as_str()).unwrap().into_raw())
+        .collect();
 
     let rc;
     unsafe {
         rc = raw::rte_eal_init(c_args.len() as c_int, c_args.as_mut_ptr());
     };
     match rc {
-        -1 => Err(Error::DpdkError(format!("DPDK EAL initialization failed",))),
+        -1 => return Err(Error::DpdkError(format!("DPDK EAL initialization failed",))),
         _ => {
             println!("DPDK EAL initialization success");
-            Ok(())
+        }
+    };
+
+    // reconstitued memory
+    for arg in c_args {
+        unsafe {
+            let _ = CString::from_raw(arg);
         }
     }
+    Ok(())
 }
 
 /// DPDK EAL cleanup
