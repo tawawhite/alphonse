09657e6dbfd0a48d3cda5e7747ef51411266b2a2
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2020-03-14 23:30:47 +0800

    better trait usage in catpure; update .gitignore

--- src/capture/mod.rs
+++ src/capture/mod.rs
@@ -1,5 +1,3 @@
-use std::error::Error;
-use std::marker::PhantomData;
 use std::path::Path;
 
 extern crate pcap;
@@ -8,23 +6,23 @@ use super::{error, packet};
 
 pub trait Backend {}
 
-pub struct Offline {}
+pub struct Offline {
+    pcap_file: pcap::Capture<pcap::Offline>,
+}
 
 impl Backend for Offline {}
 
 pub struct Capture<B: Backend> {
-    backend: PhantomData<B>,
-    pcap_file: pcap::Capture<pcap::Offline>,
+    backend: B,
 }
 
 impl Capture<Offline> {
     pub fn from_pcap_file<P: AsRef<Path>>(path: &P) -> Result<Capture<Offline>, error::Error> {
         if !path.as_ref().exists() {
             // check pcap file's existence
-            return Err(error::Error::CaptureError(String::from(format!(
-                "{} does not exists!",
-                path.as_ref().display()
-            ))));
+            return Err(error::Error::IoError(std::io::Error::from(
+                std::io::ErrorKind::NotFound,
+            )));
         }
 
         let result = pcap::Capture::from_file(path);
@@ -38,17 +36,16 @@ impl Capture<Offline> {
         }
 
         Ok(Capture {
-            backend: PhantomData,
-            pcap_file,
+            backend: Offline { pcap_file },
         })
     }
 
     #[inline]
     pub fn next(&mut self) -> Result<packet::Packet, error::Error> {
         let result;
-        match self.pcap_file.next() {
+        match self.backend.pcap_file.next() {
             Ok(raw_pkt) => result = Ok(packet::Packet::new(raw_pkt)),
-            Err(e) => result = Err(error::Error::CaptureError(String::from(e.description()))),
+            Err(e) => result = Err(error::Error::CaptureError(e)),
         }
         result
     }
