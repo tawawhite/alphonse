234555e97a6a94f575f63b10b245ef2efd822d88
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2020-06-17 19:55:31 +0800

    cmake add hyperscan external project dependency

--- cmake/projects.cmake
+++ cmake/projects.cmake
@@ -1,7 +1,12 @@
 include(ExternalProject)
 
 include(libpcap)
+include(ragel)
+include(boost)
+include(pcre)
+include(hyperscan)
 include(llhttp)
 include(dpdk)
 
 ExternalProject_Add_StepDependencies(dpdk configure libpcap)
+ExternalProject_Add_StepDependencies(hyperscan configure ragel boost libpcre)
