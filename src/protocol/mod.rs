2054327b29b6b1d0ab9608b59c16ffdf543bf38c
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2020-06-18 22:36:28 +0800

    add missing Protocol struct

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -3,3 +3,10 @@ use super::packet;
 mod classifier;
 
 pub use classifier::{Classifier, ClassifyScratch};
+
+/// Network Protocol
+#[derive(Clone, Default)]
+pub struct Protocol {
+    name: String,
+    id: u8,
+}
