af2cd06aa1778c758ce96221b82ff62b62bbf711
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2020-06-18 21:54:51 +0800

    rename Session field; Session add field

--- src/session/mod.rs
+++ src/session/mod.rs
@@ -2,33 +2,33 @@ use super::packet;
 
 /// network session
 pub struct Session {
-    pub hash: u64,
+    pub id: String,
     /// Some session only contains one direction's packets
     /// Some protocols may work in that way
     /// but network problems could cause single direction
-    pub id: String,
     pub single_direction: bool,
     /// session total bytes
     pub bytes: [u64; 2],
     /// session total data bytes
-    pub databytes: [u64; 2],
+    pub data_bytes: [u64; 2],
     /// session start time
     pub start_time: libc::timeval,
     /// session end time
     pub end_time: libc::timeval,
     /// session's packets
     pub pkts: Vec<Box<packet::Packet>>,
+    /// indicate nothing to parse here
+    pub parse_finished: bool,
 }
 
 impl Session {
     /// Create a new session
     pub fn new() -> Session {
         Session {
-            hash: 0,
             id: String::new(),
             single_direction: false,
             bytes: [0; 2],
-            databytes: [0; 2],
+            data_bytes: [0; 2],
             start_time: libc::timeval {
                 tv_sec: 0,
                 tv_usec: 0,
@@ -38,6 +38,7 @@ impl Session {
                 tv_usec: 0,
             },
             pkts: Vec::new(),
+            parse_finished: false,
         }
     }
 }
