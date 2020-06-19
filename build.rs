01a1defc970a8b4bbced683576ad346857d78baa
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2020-06-19 19:34:14 +0800

    uncomment build_dpdk()

--- build.rs
+++ build.rs
@@ -5,38 +5,39 @@ extern crate bindgen;
 #[cfg(all(target_os = "linux", feature = "dpdk"))]
 extern crate cpp_build;
 
-// fn build_dpdk() {
-//     let dpdk_bindings = bindgen::Builder::default()
-//         .header("src/ffi/dpdk.h")
-//         .clang_args(
-//             vec![
-//                 "-I",
-//                 "build/install/include/dpdk/generic",
-//                 "-I",
-//                 "build/install/include/dpdk",
-//             ]
-//             .iter(),
-//         )
-//         // rte_arp_ipv4 has some problems with the rustc compiler, it is only used by rte_qrp_hdr,
-//         // and rte_qrp_hdr is not used in any public api,
-//         // so we blacklist these two data struct to generate a valid DPDK binding
-//         .blacklist_type("rte_arp_hdr")
-//         .blacklist_type("rte_arp_ipv4")
-//         .generate_inline_functions(true)
-//         .rust_target(bindgen::LATEST_STABLE_RUST)
-//         .generate()
-//         .expect("Unable to generate dpdk bindings");
-//     dpdk_bindings
-//         .write_to_file("src/dpdk/raw.rs")
-//         .expect("Unable to generate dpdk bindings");
+#[cfg(all(target_os = "linux", feature = "dpdk"))]
+fn build_dpdk() {
+    let dpdk_bindings = bindgen::Builder::default()
+        .header("src/ffi/dpdk.h")
+        .clang_args(
+            vec![
+                "-I",
+                "build/install/include/dpdk/generic",
+                "-I",
+                "build/install/include/dpdk",
+            ]
+            .iter(),
+        )
+        // rte_arp_ipv4 has some problems with the rustc compiler, it is only used by rte_qrp_hdr,
+        // and rte_qrp_hdr is not used in any public api,
+        // so we blacklist these two data struct to generate a valid DPDK binding
+        .blacklist_type("rte_arp_hdr")
+        .blacklist_type("rte_arp_ipv4")
+        .generate_inline_functions(true)
+        .rust_target(bindgen::LATEST_STABLE_RUST)
+        .generate()
+        .expect("Unable to generate dpdk bindings");
+    dpdk_bindings
+        .write_to_file("src/dpdk/raw.rs")
+        .expect("Unable to generate dpdk bindings");
 
-//     cpp_build::Config::new()
-//         .include("src")
-//         .include("build/install/include/dpdk")
-//         .include("build/install/include/dpdk/generic/")
-//         // .flag("-mavx2")
-//         .build("src/dpdk/header.rs");
-// }
+    cpp_build::Config::new()
+        .include("src")
+        .include("build/install/include/dpdk")
+        .include("build/install/include/dpdk/generic/")
+        // .flag("-mavx2")
+        .build("src/dpdk/header.rs");
+}
 
 fn main() {
     let manifest_dir = var("CARGO_MANIFEST_DIR").unwrap();
