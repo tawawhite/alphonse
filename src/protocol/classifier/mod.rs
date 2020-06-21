484c43fc74287db198264ef6b9dc846e5d31f680
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2020-06-21 21:33:49 +0800

    implement add_dpi_rule and add unittest; fix dpi classifier logic error

--- src/protocol/classifier/mod.rs
+++ src/protocol/classifier/mod.rs
@@ -6,6 +6,14 @@ use hyperscan::{BlockDatabase, Builder};
 use super::packet;
 use super::Protocol;
 
+type ProtocolID = u8;
+
+#[derive(Clone)]
+struct DpiRule {
+    hs_pattern: hyperscan::Pattern,
+    protocols: Vec<ProtocolID>,
+}
+
 /// Protocol Classifier
 pub struct Classifier {
     protocol_table: Box<Vec<Protocol>>,
@@ -16,7 +24,7 @@ pub struct Classifier {
     udp_port_protocols: Box<Vec<Vec<u8>>>,
     sctp_port_protocols: Box<Vec<Vec<u8>>>,
 
-    dpi_rules: Box<Vec<hyperscan::Pattern>>,
+    dpi_rules: Box<Vec<DpiRule>>,
 
     hs_db: Option<BlockDatabase>,
 }
@@ -86,15 +94,48 @@ impl Classifier {
         };
     }
 
-    pub fn add_dpi_rule(&mut self, rule: hyperscan::Pattern) {
-        self.dpi_rules.push(rule);
+    pub fn add_dpi_rule(&mut self, pattern: hyperscan::Pattern, protocol_id: ProtocolID) {
+        let mut same_pattern_index: i32 = -1;
+        for (i, dpi_rule) in (&*self.dpi_rules).iter().enumerate() {
+            if dpi_rule.hs_pattern.expression == pattern.expression {
+                same_pattern_index = i as i32;
+                break;
+            }
+        }
+
+        if same_pattern_index < 0 {
+            let dpi_rule = DpiRule {
+                hs_pattern: pattern,
+                protocols: vec![protocol_id],
+            };
+            self.dpi_rules.push(dpi_rule);
+        } else {
+            let dpi_rule = self.dpi_rules.get_mut(same_pattern_index as usize).unwrap();
+            let mut id_conflict = false;
+            for p_id in &dpi_rule.protocols {
+                if *p_id == protocol_id {
+                    id_conflict = true;
+                    break;
+                }
+            }
+            if !id_conflict {
+                dpi_rule.protocols.push(protocol_id);
+            } else {
+                panic!("DpiRule protocol id conflict!");
+            }
+        }
     }
 
     pub fn prepare(&mut self) {
         if self.dpi_rules.len() == 0 {
             self.hs_db = None
         } else {
-            let patterns = hyperscan::Patterns::from_iter(self.dpi_rules.clone().into_iter());
+            let patterns = hyperscan::Patterns::from_iter(
+                (*self.dpi_rules.clone())
+                    .into_iter()
+                    .map(|r| r.hs_pattern)
+                    .into_iter(),
+            );
             self.hs_db = Some(patterns.build().unwrap());
         }
     }
@@ -111,13 +152,80 @@ impl Classifier {
             (Some(db), Some(s)) => Ok(db.scan(
                 &pkt.data.as_slice()[pkt.app_layer.offset as usize..],
                 s,
-                |id, from, to, _flags| {
-                    println!("found pattern #{} @ [{}, {})", id, from, to);
-                    protocols.push(id as u8);
+                |id, _from, _to, _flags| {
+                    // !!! use direct access for performance, very dangerous, may panic !!!
+                    let dpi_rule = &self.dpi_rules[id as usize];
+                    protocols.extend(&dpi_rule.protocols);
                     hyperscan::Matching::Continue
                 },
             )?),
             _ => Ok(()),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn classifier_add_all_pkt_rule() {
+        let mut classifier = Classifier::new();
+        classifier.add_all_pkt_rule(0);
+        assert_eq!(classifier.all_pkt_protocols.len(), 1);
+        assert_eq!(classifier.all_pkt_protocols[0], 0);
+    }
+
+    #[test]
+    fn classifier_add_port_rule() {
+        let mut classifier = Classifier::new();
+        classifier.add_port_rule(0, 443, packet::Protocol::TCP);
+        assert_eq!(classifier.tcp_port_protocols[443][0], 0);
+
+        classifier.add_port_rule(100, 80, packet::Protocol::UDP);
+        classifier.add_port_rule(200, 80, packet::Protocol::UDP);
+        assert_eq!(classifier.udp_port_protocols[80][0], 100);
+        assert_eq!(classifier.udp_port_protocols[80][1], 200);
+
+        classifier.add_port_rule(4, 400, packet::Protocol::SCTP);
+        assert_eq!(classifier.sctp_port_protocols[400][0], 4);
+    }
+
+    #[test]
+    fn classifier_add_dpi_rule() {
+        let mut classifier = Classifier::new();
+        let expression = String::from("regex");
+        let pattern = hyperscan::Pattern::new(expression.clone()).unwrap();
+        classifier.add_dpi_rule(pattern, 100);
+        assert_eq!(classifier.dpi_rules[0].hs_pattern.expression, expression);
+        assert_eq!(classifier.dpi_rules[0].protocols[0], 100);
+
+        classifier.prepare();
+        let scratch = classifier.alloc_scratch().unwrap();
+        let mut protocols = Vec::new();
+        let mut pkt = packet::Packet::default();
+
+        classifier.classify(&pkt, &mut protocols, &scratch).unwrap();
+        assert_eq!(protocols.len(), 0);
+
+        pkt.data = Box::new(Vec::from(String::from("regex").as_bytes()));
+        classifier.classify(&pkt, &mut protocols, &scratch).unwrap();
+        assert_eq!(protocols.len(), 1);
+        println!("result: {:?}", protocols);
+        assert_eq!(protocols[0], 100);
+    }
+
+    fn classifier_add_dpi_rule_with_existing_expression() {
+        let mut classifier = Classifier::new();
+        let expression = String::from("regex");
+        let pattern = hyperscan::Pattern::new(expression.clone()).unwrap();
+        classifier.add_dpi_rule(pattern, 100);
+        assert_eq!(classifier.dpi_rules[0].hs_pattern.expression, expression);
+        assert_eq!(classifier.dpi_rules[0].protocols[0], 100);
+
+        let expression = String::from("regex");
+        let pattern = hyperscan::Pattern::new(expression.clone()).unwrap();
+        classifier.add_dpi_rule(pattern, 8);
+        assert_eq!(classifier.dpi_rules[0].hs_pattern.expression, expression);
+        assert_eq!(classifier.dpi_rules[0].protocols[1], 8);
     }
 }
