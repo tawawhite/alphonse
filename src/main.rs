8a03dd5490481da1668a604d2863dd19b0062a90
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2020-06-24 18:49:44 +0800

    remove dpdk related logic in main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,61 +19,11 @@ use crossbeam_channel::unbounded;
 mod capture;
 mod commands;
 mod config;
-#[cfg(all(target_os = "linux", feature = "dpdk"))]
-mod dpdk;
 mod packet;
 mod protocol;
 mod session;
 mod threadings;
 
-#[cfg(all(target_os = "linux", feature = "dpdk"))]
-fn main() -> Result<()> {
-    let root_cmd = commands::new_root_command();
-
-    let mut config = config::parse_args(root_cmd)?;
-
-    match dpdk::eal_init(&mut config.dpdk_eal_args) {
-        Err(e) => {
-            dpdk::eal_cleanup();
-            return Err(e);
-        }
-        Ok(_) => {}
-    };
-
-    let parser_result = protocols::Parser::from_pcap_file(&config.pcap_file);
-    let mut parser;
-    match parser_result {
-        Err(e) => {
-            dpdk::eal_cleanup();
-            return Err(e);
-        }
-        Ok(p) => parser = p,
-    }
-
-    let cap_result = capture::Capture::from_pcap_file(&config.pcap_file);
-    let mut cap;
-    match cap_result {
-        Err(e) => {
-            dpdk::eal_cleanup();
-            return Err(e);
-        }
-        Ok(c) => cap = c,
-    }
-
-    while let Ok(mut pkt) = cap.next() {
-        let result = parser.parse_pkt(&mut pkt);
-        match result {
-            Ok(_) => {}
-            Err(e) => println!("{:?}", e),
-        }
-    }
-
-    dpdk::eal_cleanup();
-
-    Ok(())
-}
-
-#[cfg(not(feature = "dpdk"))]
 fn main() -> Result<()> {
     let root_cmd = commands::new_root_command();
     let cfg = config::parse_args(root_cmd)?;
