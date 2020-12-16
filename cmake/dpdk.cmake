57be499c3382b43635bc26dd74316dc1fde62672
Jack Liar <>
2020-12-16 18:10:01 +0800

    disable kmods and test for dpdk build; comment igb_uio vmware support trick

--- cmake/dpdk.cmake
+++ cmake/dpdk.cmake
@@ -14,8 +14,8 @@ ExternalProject_Add(dpdk
         LIBRARY_PATH=${CMAKE_CURRENT_BINARY_DIR}/install/lib:${CMAKE_CURRENT_BINARY_DIR}/install/lib64
         C_INCLUDE_PATH=${CMAKE_CURRENT_BINARY_DIR}/install/include
         PKG_CONFIG_PATH=${CMAKE_CURRENT_BINARY_DIR}/install/lib/pkgconfig
-        meson setup -Dprefix=<INSTALL_DIR> --includedir=${CMAKE_INSTALL_INCLUDEDIR}/dpdk --default-library=shared <BINARY_DIR> <SOURCE_DIR>
-    COMMAND sed -i.bak -e "s/supported(udev->pdev)/supported(udev->pdev)||true/g" <SOURCE_DIR>/kernel/linux/igb_uio/igb_uio.c
+        meson setup -Denable_kmods=false -Dtests=false -Dprefix=<INSTALL_DIR> --includedir=${CMAKE_INSTALL_INCLUDEDIR}/dpdk --default-library=shared <BINARY_DIR> <SOURCE_DIR>
+    # COMMAND sed -i.bak -e "s/supported(udev->pdev)/supported(udev->pdev)||true/g" <SOURCE_DIR>/kernel/linux/igb_uio/igb_uio.c
     BUILD_COMMAND ${CMAKE_COMMAND} -E env
         LIBRARY_PATH=${CMAKE_CURRENT_BINARY_DIR}/install/lib:${CMAKE_CURRENT_BINARY_DIR}/install/lib64
         C_INCLUDE_PATH=${CMAKE_CURRENT_BINARY_DIR}/install/include
