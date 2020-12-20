1c4e350ec4d33d1dcaa4219b9522e18820b2a40b
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2020-12-20 22:02:01 +0800

    update build.sh & README.md

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # alphonse
 
 [![dependency status](https://deps.rs/repo/github/jackliar/alphonse/status.svg)](https://deps.rs/repo/github/jackliar/alphonse)
+
+
+## Build
+
+### Build Prerequisite
+
+1. Install [cmake](https://cmake.org/download/) (**Strongly suggest**)
+
+### Build alphonse
+
+1. Execute build.sh
--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #! /bin/sh
-mkdir -p build && cd build && cmake .. && make && make install
-cargo build --verbose --all
-cargo test --verbose --all
+mkdir -p build && cd build && cmake .. && make && make install && cd ..
+
+export PKG_CONFIG_PATH=$(pwd)/build/install/lib/pkgconfig:$(pwd)/build/install/lib64/pkgconfig
+export LD_LIBRARY_PATH=$(pwd)/build/install/lib
+export RUSTC_VERSION=$(rustc --version | awk '{print $2}')
+
+echo $PKG_CONFIG_PATH
+cargo build --all
+cargo test --all
+
+unset PKG_CONFIG_PATH
+unset LD_LIBRARY_PATH
+unset RUSTC_VERSION
