69a8cbcee3be2fd1ff9cbb06d24e74076273e496
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-01-05 21:29:52 +0800

    remove unused import

--- app/src/threadings/session.rs
+++ app/src/threadings/session.rs
@@ -3,30 +3,9 @@ use std::sync::Arc;
 
 use anyhow::Result;
 use crossbeam_channel::Receiver;
-use fnv::FnvHashMap;
-
-use alphonse_api as api;
-use api::classifiers::ClassifierManager;
-use api::packet::Packet;
-use api::parsers::{ParserID, ProtocolParserTrait};
 
 use super::config;
 use super::sessions::Session;
-
-/// Data structure to store session info and session's protocol parsers
-struct SessionData {
-    info: Box<Session>,
-    parsers: Box<FnvHashMap<ParserID, Box<dyn ProtocolParserTrait>>>,
-}
-
-impl SessionData {
-    fn new() -> Self {
-        SessionData {
-            info: Box::new(Session::new()),
-            parsers: Box::new(FnvHashMap::default()),
-        }
-    }
-}
 
 /// 数据包处理线程
 pub struct SessionThread {
