a606932ad9576cf10ff57f381e83ce1b3ddfa71e
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-01-11 22:14:08 +0800

    add missing field in CaptureStat

--- app/src/stats.rs
+++ app/src/stats.rs
@@ -2,7 +2,9 @@
 #[derive(Debug, Default)]
 pub struct CaptureStat {
     /// Total received packets
-    pub received: u64,
+    pub rx_pkts: u64,
+    /// Total received bytes
+    pub rx_bytes: u64,
     /// Total dropped packets
     pub dropped: u64,
     /// Total dropped packets by network interface
