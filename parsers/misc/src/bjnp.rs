5a8eff8689e58b67315ff491adf8fc9df27d5d4c
Jack Liar <>
2021-02-16 23:59:54 +0800

    bjnp bind udp dpi rule

--- parsers/misc/src/bjnp.rs
+++ parsers/misc/src/bjnp.rs
@@ -14,7 +14,7 @@ pub fn register_classify_rules(
     match_cbs: &mut FnvHashMap<RuleID, MatchCallBack>,
 ) -> Result<()> {
     let mut dpi_rule = dpi::Rule::new(pattern! {r"^BJNP"});
-    dpi_rule.protocol = dpi::Protocol::TCP;
+    dpi_rule.protocol = dpi::Protocol::UDP;
     let mut rule = Rule::new(id);
     rule.rule_type = RuleType::DPI(dpi_rule);
     let rule_id = manager.add_rule(&mut rule)?;
@@ -43,7 +43,7 @@ mod test {
         // pattern 1
         let mut pkt: Box<TestPacket> = Box::new(TestPacket::default());
         pkt.raw = Box::new(b"BJNP".to_vec());
-        pkt.layers.trans.protocol = Protocol::TCP;
+        pkt.layers.trans.protocol = Protocol::UDP;
         let mut pkt: Box<dyn api::packet::Packet> = pkt;
         manager.classify(&mut pkt, &mut scratch).unwrap();
         assert_eq!(pkt.rules().len(), 1);
