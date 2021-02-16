194d25c259b23c7bdcce004665c91625c0c45bf9
Jack Liar <>
2021-02-16 22:53:34 +0800

    add thrift misc parser

---
+++ parsers/misc/src/thrift.rs
@@ -0,0 +1,83 @@
+use anyhow::Result;
+use fnv::FnvHashMap;
+use hyperscan::pattern;
+
+use alphonse_api as api;
+use api::classifiers::{dpi, ClassifierManager, Rule, RuleID, RuleType};
+use api::packet::Packet;
+use api::parsers::ParserID;
+use api::session::Session;
+
+use super::MatchCallBack;
+
+pub fn register_classify_rules(
+    id: ParserID,
+    manager: &mut ClassifierManager,
+    match_cbs: &mut FnvHashMap<RuleID, MatchCallBack>,
+) -> Result<()> {
+    let mut dpi_rule = dpi::Rule::new(pattern! {r"^\x80\x01\x00\x01\x00\x00\x00"});
+    dpi_rule.protocol = dpi::Protocol::TCP;
+    let mut rule = Rule::new(id);
+    rule.rule_type = RuleType::DPI(dpi_rule);
+    let rule_id = manager.add_rule(&mut rule)?;
+    match_cbs.insert(rule_id, MatchCallBack::ProtocolName("thrift".to_string()));
+
+    let mut dpi_rule = dpi::Rule::new(pattern! {r"^\x00\x00..\x80\x01\x00"});
+    dpi_rule.protocol = dpi::Protocol::TCP;
+    let mut rule = Rule::new(id);
+    rule.rule_type = RuleType::DPI(dpi_rule);
+    let rule_id = manager.add_rule(&mut rule)?;
+    match_cbs.insert(rule_id, MatchCallBack::Func(classify));
+
+    Ok(())
+}
+
+fn classify(ses: &mut Session, pkt: &Box<dyn Packet>) {
+    if pkt.payload().len() <= 20 {
+        return;
+    }
+    ses.add_protocol("thrift");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use api::packet::Protocol;
+    use api::session::Session;
+    use api::{parsers::ProtocolParserTrait, utils::packet::Packet as TestPacket};
+
+    use crate::ProtocolParser;
+
+    #[test]
+    fn thrift() {
+        let mut manager = ClassifierManager::new();
+        let mut parser = ProtocolParser::default();
+        parser.register_classify_rules(&mut manager).unwrap();
+        manager.prepare().unwrap();
+        let mut scratch = manager.alloc_scratch().unwrap();
+
+        // rule1
+        let mut pkt: Box<TestPacket> = Box::new(TestPacket::default());
+        pkt.raw = Box::new(b"\x80\x01\x00\x01\x00\x00\x00".to_vec());
+        pkt.layers.trans.protocol = Protocol::TCP;
+        let mut pkt: Box<dyn api::packet::Packet> = pkt;
+        manager.classify(&mut pkt, &mut scratch).unwrap();
+        assert_eq!(pkt.rules().len(), 1);
+
+        let mut ses = Session::new();
+        parser.parse_pkt(&pkt, &pkt.rules()[0], &mut ses).unwrap();
+        assert!(ses.has_protocol("thrift"));
+
+        // rule2
+        let mut pkt: Box<TestPacket> = Box::new(TestPacket::default());
+        pkt.raw = Box::new(b"\x00\x00\x02\x03\x80\x01\x00890123456789012345".to_vec());
+        pkt.layers.trans.protocol = Protocol::TCP;
+        let mut pkt: Box<dyn api::packet::Packet> = pkt;
+        manager.classify(&mut pkt, &mut scratch).unwrap();
+        assert_eq!(pkt.rules().len(), 1);
+
+        let mut ses = Session::new();
+        parser.parse_pkt(&pkt, &pkt.rules()[0], &mut ses).unwrap();
+        assert!(ses.has_protocol("thrift"));
+    }
+}
