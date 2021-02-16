928004611cd34b6b140feb56123cef3ff2696f8a
Jack Liar <>
2021-02-17 00:18:13 +0800

    add new rule for stun misc parser

--- parsers/misc/src/stun.rs
+++ parsers/misc/src/stun.rs
@@ -20,6 +20,13 @@ pub fn register_classify_rules(
     let rule_id = manager.add_rule(&mut rule)?;
     match_cbs.insert(rule_id, MatchCallBack::ProtocolName("stun".to_string()));
 
+    let mut dpi_rule = dpi::Rule::new(pattern! {r"^((\x00\x01)|(\x0\x03)|(\x01\x01))\x00"});
+    dpi_rule.protocol = dpi::Protocol::UDP;
+    let mut rule = Rule::new(id);
+    rule.rule_type = RuleType::DPI(dpi_rule);
+    let rule_id = manager.add_rule(&mut rule)?;
+    match_cbs.insert(rule_id, MatchCallBack::ProtocolName("stun".to_string()));
+
     Ok(())
 }
 
@@ -40,9 +47,46 @@ mod test {
         manager.prepare().unwrap();
         let mut scratch = manager.alloc_scratch().unwrap();
 
+        // rule 1
         let mut pkt: Box<TestPacket> = Box::new(TestPacket::default());
         pkt.raw = Box::new(b"RSP/   STUN".to_vec());
         pkt.layers.trans.protocol = Protocol::TCP;
+        let mut pkt: Box<dyn api::packet::Packet> = pkt;
+        manager.classify(&mut pkt, &mut scratch).unwrap();
+        assert_eq!(pkt.rules().len(), 1);
+
+        let mut ses = Session::new();
+        parser.parse_pkt(&pkt, &pkt.rules()[0], &mut ses).unwrap();
+        assert!(ses.has_protocol("stun"));
+
+        // rule 2 pattern 1
+        let mut pkt: Box<TestPacket> = Box::new(TestPacket::default());
+        pkt.raw = Box::new(b"\x00\x01\x00".to_vec());
+        pkt.layers.trans.protocol = Protocol::UDP;
+        let mut pkt: Box<dyn api::packet::Packet> = pkt;
+        manager.classify(&mut pkt, &mut scratch).unwrap();
+        assert_eq!(pkt.rules().len(), 1);
+
+        let mut ses = Session::new();
+        parser.parse_pkt(&pkt, &pkt.rules()[0], &mut ses).unwrap();
+        assert!(ses.has_protocol("stun"));
+
+        // rule 2 pattern 2
+        let mut pkt: Box<TestPacket> = Box::new(TestPacket::default());
+        pkt.raw = Box::new(b"\x00\x03\x00".to_vec());
+        pkt.layers.trans.protocol = Protocol::UDP;
+        let mut pkt: Box<dyn api::packet::Packet> = pkt;
+        manager.classify(&mut pkt, &mut scratch).unwrap();
+        assert_eq!(pkt.rules().len(), 1);
+
+        let mut ses = Session::new();
+        parser.parse_pkt(&pkt, &pkt.rules()[0], &mut ses).unwrap();
+        assert!(ses.has_protocol("stun"));
+
+        // rule 2 pattern 3
+        let mut pkt: Box<TestPacket> = Box::new(TestPacket::default());
+        pkt.raw = Box::new(b"\x01\x01\x00".to_vec());
+        pkt.layers.trans.protocol = Protocol::UDP;
         let mut pkt: Box<dyn api::packet::Packet> = pkt;
         manager.classify(&mut pkt, &mut scratch).unwrap();
         assert_eq!(pkt.rules().len(), 1);
