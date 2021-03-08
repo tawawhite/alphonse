4dadfbda3c0d4fc065a182f7415abfb139ff4924
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-03-08 21:26:15 +0800

    travis-ci build on multiple CPU architectures

--- cmake/hyperscan.cmake
+++ cmake/hyperscan.cmake
@@ -1,14 +1,26 @@
 if(OFFLINE_ENVIRONMENT)
-    set(hyperscan_url ${CMAKE_CURRENT_SOURCE_DIR}/third_party/hyperscan-5.4.0.tar.gz)
+    if (CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+        set(hyperscan_url ${CMAKE_CURRENT_SOURCE_DIR}/third_party/hyperscan-5.4.0.tar.gz)
+        set(url_md5 65e08385038c24470a248f6ff2fa379b)
+    elseif (CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+        set(hyperscan_url ${CMAKE_CURRENT_SOURCE_DIR}/third_party/hyperscan-5.3.0.aarch64.tar.gz)
+        set(url_md5 ef337257bde6583242a739fab6fb161f)
+    endif()
 else()
-    set(hyperscan_url https://github.com/intel/hyperscan/archive/v5.4.0.tar.gz)
+    if (CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+        set(hyperscan_url https://github.com/intel/hyperscan/archive/v5.4.0.tar.gz)
+        set(url_md5 65e08385038c24470a248f6ff2fa379b)
+    elseif (CMAKE_SYSTEM_PROCESSOR STREQUAL "aarch64")
+        set(hyperscan_url https://github.com/kunpengcompute/hyperscan/archive/v5.3.0.aarch64.tar.gz)
+        set(url_md5 ef337257bde6583242a739fab6fb161f)
+    endif()
 endif()
 
 set(FLAGS "-I${CMAKE_CURRENT_BINARY_DIR}/install/include -L${CMAKE_CURRENT_BINARY_DIR}/install/${CMAKE_INSTALL_LIBDIR}")
 
 ExternalProject_Add(hyperscan
     URL ${hyperscan_url}
-    URL_MD5 65e08385038c24470a248f6ff2fa379b
+    URL_MD5 ${url_md5}
     EXCLUDE_FROM_ALL ON
     PREFIX hyperscan
     INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/install
