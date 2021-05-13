48daa8f91445cf201c1227c2298ed5e763412534
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-05-13 20:48:24 +0800

    fix org name pushed into common name error

--- parsers/dtls/src/lib.rs
+++ parsers/dtls/src/lib.rs
@@ -12,7 +12,7 @@ use api::{add_simple_dpi_rule, add_simple_dpi_udp_rule};
 
 #[derive(Debug, Default, Clone)]
 struct CertInfo {
-    comman_name: Vec<String>,
+    common_name: Vec<String>,
     org_name: Vec<String>,
 }
 
@@ -177,14 +177,14 @@ impl Processor {
                     // get issuer information
                     for cn in cer.issuer().iter_common_name() {
                         match cn.as_str() {
-                            Ok(cn) => cert.issuer.comman_name.push(cn.to_string()),
+                            Ok(cn) => cert.issuer.common_name.push(cn.to_string()),
                             Err(_) => continue,
                         }
                     }
 
                     for org in cer.issuer().iter_organization() {
                         match org.as_str() {
-                            Ok(org) => cert.issuer.comman_name.push(org.to_string()),
+                            Ok(org) => cert.issuer.org_name.push(org.to_string()),
                             Err(_) => continue,
                         }
                     }
@@ -196,14 +196,14 @@ impl Processor {
                     // get subject information
                     for cn in cer.subject().iter_common_name() {
                         match cn.as_str() {
-                            Ok(cn) => cert.issuer.comman_name.push(cn.to_string()),
+                            Ok(cn) => cert.issuer.common_name.push(cn.to_string()),
                             Err(_) => continue,
                         }
                     }
 
                     for org in cer.subject().iter_organization() {
                         match org.as_str() {
-                            Ok(org) => cert.issuer.comman_name.push(org.to_string()),
+                            Ok(org) => cert.issuer.org_name.push(org.to_string()),
                             Err(_) => continue,
                         }
                     }
