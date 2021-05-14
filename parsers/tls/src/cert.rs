cfc45e40a9b84f5d02f93eac2797e556cf756ed4
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-05-14 19:58:48 +0800

    implement Serialize for Cert and CertInfo; fix subject assign error

--- parsers/tls/src/cert.rs
+++ parsers/tls/src/cert.rs
@@ -1,12 +1,105 @@
+use std::collections::HashSet;
+
+use serde::ser::SerializeStruct;
+use serde::{Serialize, Serializer};
 use tls_parser::TlsCertificateContents;
 use x509_parser::prelude::{parse_x509_certificate, GeneralName, ParsedExtension};
 
 use crate::Processor;
 
-impl<'a> Processor<'static> {
+#[derive(Clone, Debug, Default, Serialize)]
+pub struct CertInfo {
+    common_name: HashSet<String>,
+    org_name: HashSet<String>,
+}
+
+fn serialize_issuer<S>(issuer: &CertInfo, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let mut state = s.serialize_struct("", 2)?;
+    if !issuer.org_name.is_empty() {
+        state.serialize_field("issuerON", &issuer.org_name)?;
+    }
+    if !issuer.common_name.is_empty() {
+        state.serialize_field("issuerCN", &issuer.common_name)?;
+    }
+    state.end()
+}
+
+fn serialize_subject<S>(subject: &CertInfo, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let mut state = s.serialize_struct("", 2)?;
+    if !subject.org_name.is_empty() {
+        state.serialize_field("subjectON", &subject.org_name)?;
+    }
+    if !subject.common_name.is_empty() {
+        state.serialize_field("subjectCN", &subject.common_name)?;
+    }
+    state.end()
+}
+
+#[derive(Clone, Debug, Default, Serialize)]
+pub struct Cert {
+    #[serde(skip_serializing)]
+    pub hash: u32,
+
+    #[serde(rename = "notBefore")]
+    pub not_before: u64,
+
+    #[serde(rename = "notAfter")]
+    pub not_after: u64,
+
+    #[serde(flatten)]
+    #[serde(serialize_with = "serialize_issuer")]
+    pub issuer: CertInfo,
+
+    #[serde(flatten)]
+    #[serde(serialize_with = "serialize_subject")]
+    pub subject: CertInfo,
+
+    #[serde(skip_serializing_if = "HashSet::is_empty")]
+    pub alt: HashSet<String>,
+
+    #[serde(rename = "serial")]
+    pub serial_number: String,
+
+    #[serde(skip_serializing)]
+    pub bucket: usize,
+
+    #[serde(rename = "hash")]
+    pub hash_str: String,
+
+    #[serde(skip_serializing)]
+    pub is_ca: bool,
+
+    #[serde(skip_serializing_if = "String::is_empty")]
+    pub algorithm: String,
+
+    #[serde(skip_serializing_if = "String::is_empty")]
+    pub curv: String,
+
+    #[serde(rename = "validDays")]
+    pub valid_days: usize,
+}
+
+impl Cert {
+    fn update_valid_days(&mut self) {
+        let not_before = std::time::UNIX_EPOCH + std::time::Duration::from_millis(self.not_before);
+        let not_before = chrono::DateTime::<chrono::Utc>::from(not_before);
+        let not_after = std::time::UNIX_EPOCH + std::time::Duration::from_millis(self.not_after);
+        let not_after = chrono::DateTime::<chrono::Utc>::from(not_after);
+        let diff = not_after - not_before;
+        self.valid_days = diff.num_days() as usize;
+    }
+}
+
+impl Processor {
     pub fn handle_certificate(&mut self, cert: &TlsCertificateContents) {
         for raw_cert in &cert.cert_chain {
-            let mut cert = crate::Cert::default();
+            let mut cert = Cert::default();
             match parse_x509_certificate(raw_cert.data) {
                 Err(_) => continue,
                 Ok((_, cer)) => {
@@ -33,14 +126,14 @@ impl<'a> Processor<'static> {
                     }
 
                     // get validity information
-                    cert.not_before = cer.validity().not_before.timestamp() as u64;
-                    cert.not_after = cer.validity().not_after.timestamp() as u64;
+                    cert.not_before = cer.validity().not_before.timestamp() as u64 * 1000;
+                    cert.not_after = cer.validity().not_after.timestamp() as u64 * 1000;
 
                     // get subject information
                     for cn in cer.subject().iter_common_name() {
                         match cn.as_str() {
                             Ok(cn) => {
-                                cert.issuer.common_name.insert(cn.to_string());
+                                cert.subject.common_name.insert(cn.to_string());
                             }
                             Err(_) => continue,
                         }
@@ -49,7 +142,7 @@ impl<'a> Processor<'static> {
                     for org in cer.subject().iter_organization() {
                         match org.as_str() {
                             Ok(org) => {
-                                cert.issuer.org_name.insert(org.to_string());
+                                cert.subject.org_name.insert(org.to_string());
                             }
                             Err(_) => continue,
                         }
@@ -75,6 +168,7 @@ impl<'a> Processor<'static> {
                 }
             }
 
+            cert.update_valid_days();
             self.certs.push(cert);
         }
     }
