642a581ac11d56d997991bcdff4fb3b26c9608ba
Jack <15205739+JackLiar@users.noreply.github.com>
2021-05-17 13:37:59 +0800

    add fields into session; use singleton geo db

--- parsers/ip/src/lib.rs
+++ parsers/ip/src/lib.rs
@@ -1,21 +1,41 @@
-use std::hash::{BuildHasher, Hash, Hasher};
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 use anyhow::{anyhow, Result};
 use maxminddb::{geoip2, Reader as GeoLiteReader};
 use memmap::Mmap;
 use once_cell::sync::OnceCell;
+use serde::Serialize;
+use serde_json::json;
 
 use alphonse_api as api;
 use api::add_protocol_rule;
 use api::classifiers::{self, protocol, Rule, RuleType};
-use api::packet::{PacketHashKey, Protocol};
+use api::packet::Protocol;
 use api::parsers::ParserID;
 use api::session::Session;
 
-static ASN_DBS: OnceCell<Vec<GeoLiteReader<Mmap>>> = OnceCell::new();
-static COUNTRY_DBS: OnceCell<Vec<GeoLiteReader<Mmap>>> = OnceCell::new();
-static CITY_DBS: OnceCell<Vec<GeoLiteReader<Mmap>>> = OnceCell::new();
+static ASN_DB: OnceCell<GeoLiteReader<Mmap>> = OnceCell::new();
+static COUNTRY_DB: OnceCell<GeoLiteReader<Mmap>> = OnceCell::new();
+static CITY_DB: OnceCell<GeoLiteReader<Mmap>> = OnceCell::new();
+
+#[derive(Clone, Debug, Serialize)]
+struct IpInfo {
+    addr: IpAddr,
+    asn: String,
+    country: String,
+    city: String,
+}
+
+impl Default for IpInfo {
+    fn default() -> Self {
+        Self {
+            addr: IpAddr::V4(Ipv4Addr::from(0)),
+            asn: String::default(),
+            country: String::default(),
+            city: String::default(),
+        }
+    }
+}
 
 #[derive(Default)]
 struct Processor {
@@ -23,8 +43,8 @@ struct Processor {
     name: String,
     classified: bool,
     processed: bool,
-    /// Hasher to decide which thread this processor belongs to
-    hasher: fnv::FnvHasher,
+    src_ip: IpInfo,
+    dst_ip: IpInfo,
 }
 
 impl Clone for Processor {
@@ -34,7 +54,8 @@ impl Clone for Processor {
             name: self.name.clone(),
             classified: self.classified,
             processed: self.processed,
-            hasher: fnv::FnvBuildHasher::default().build_hasher(),
+            src_ip: self.src_ip.clone(),
+            dst_ip: self.dst_ip.clone(),
         }
     }
 }
@@ -60,33 +81,18 @@ impl api::parsers::ProtocolParserTrait for Processor {
     }
 
     fn init(&mut self, alcfg: &api::config::Config) -> Result<()> {
-        let mut dbs = vec![];
-        for _ in 0..alcfg.pkt_threads {
-            let db = GeoLiteReader::open_mmap("./etc/GeoLite2-ASN.mmdb")?;
-            dbs.push(db);
-        }
-        ASN_DBS
-            .set(dbs)
+        ASN_DB
+            .set(GeoLiteReader::open_mmap("./etc/GeoLite2-ASN.mmdb")?)
             .ok()
             .ok_or(anyhow!("{} ASN_DBS are already set", self.name()))?;
 
-        let mut dbs = vec![];
-        for _ in 0..alcfg.pkt_threads {
-            let db = GeoLiteReader::open_mmap("./etc/GeoLite2-Country.mmdb")?;
-            dbs.push(db);
-        }
-        COUNTRY_DBS
-            .set(dbs)
+        COUNTRY_DB
+            .set(GeoLiteReader::open_mmap("./etc/GeoLite2-Country.mmdb")?)
             .ok()
             .ok_or(anyhow!("{} COUNTRY_DBS are already set", self.name()))?;
 
-        let mut dbs = vec![];
-        for _ in 0..alcfg.pkt_threads {
-            let db = GeoLiteReader::open_mmap("./etc/GeoLite2-City.mmdb")?;
-            dbs.push(db);
-        }
-        CITY_DBS
-            .set(dbs)
+        CITY_DB
+            .set(GeoLiteReader::open_mmap("./etc/GeoLite2-City.mmdb")?)
             .ok()
             .ok_or(anyhow!("{} CITY_DBS are already set", self.name()))?;
 
@@ -130,7 +136,7 @@ impl api::parsers::ProtocolParserTrait for Processor {
             }
         }
 
-        let src_ip = unsafe {
+        self.src_ip.addr = unsafe {
             match pkt.layers().network.protocol {
                 Protocol::IPV4 => IpAddr::V4(Ipv4Addr::from(pkt.src_ipv4())),
                 Protocol::IPV6 => IpAddr::V6(Ipv6Addr::from(*pkt.src_ipv6())),
@@ -138,7 +144,7 @@ impl api::parsers::ProtocolParserTrait for Processor {
             }
         };
 
-        let dst_ip = unsafe {
+        self.dst_ip.addr = unsafe {
             match pkt.layers().network.protocol {
                 Protocol::IPV4 => IpAddr::V4(Ipv4Addr::from(pkt.dst_ipv4())),
                 Protocol::IPV6 => IpAddr::V6(Ipv6Addr::from(*pkt.dst_ipv6())),
@@ -148,30 +154,90 @@ impl api::parsers::ProtocolParserTrait for Processor {
             }
         };
 
-        PacketHashKey::from(pkt).hash(&mut self.hasher);
-        let dbs = unsafe {
-            ASN_DBS
-                .get()
-                .ok_or(anyhow!("{}: ASN_DBS is not initialized", self.name()))?
-        };
-        let hash = self.hasher.finish() as usize % dbs.len();
+        let db = ASN_DB
+            .get()
+            .ok_or(anyhow!("{}: ASN_DBS is not initialized", self.name()))?;
 
-        let src_asn: Option<geoip2::Asn> = dbs[hash].lookup(src_ip).ok();
-        let dst_asn: Option<geoip2::Asn> = dbs[hash].lookup(dst_ip).ok();
+        let asn: Option<geoip2::Asn> = db.lookup(self.src_ip.addr).ok();
+        self.src_ip.asn = asn_to_string(&asn);
+        let asn: Option<geoip2::Asn> = db.lookup(self.dst_ip.addr).ok();
+        self.dst_ip.asn = asn_to_string(&asn);
 
-        let src_country: Option<geoip2::Country> = dbs[hash].lookup(src_ip).ok();
-        let dst_country: Option<geoip2::Country> = dbs[hash].lookup(dst_ip).ok();
+        let db = COUNTRY_DB
+            .get()
+            .ok_or(anyhow!("{}: ASN_DBS is not initialized", self.name()))?;
+        let country: Option<geoip2::Country> = db.lookup(self.src_ip.addr).ok();
+        self.src_ip.country = country_to_string(&country);
+        let country: Option<geoip2::Country> = db.lookup(self.dst_ip.addr).ok();
+        self.dst_ip.country = country_to_string(&country);
 
-        let src_city: Option<geoip2::City> = dbs[hash].lookup(src_ip).ok();
-        let dst_city: Option<geoip2::City> = dbs[hash].lookup(dst_ip).ok();
+        let db = CITY_DB
+            .get()
+            .ok_or(anyhow!("{}: ASN_DBS is not initialized", self.name()))?;
+        let src_city: Option<geoip2::City> = db.lookup(self.src_ip.addr).ok();
+        self.src_ip.city = city_to_string(&src_city);
+        let dst_city: Option<geoip2::City> = db.lookup(self.dst_ip.addr).ok();
+        self.dst_ip.city = city_to_string(&dst_city);
 
-        println!("{:?} {:?} {:?}", src_asn, src_country, src_city);
-        println!("{:?} {:?} {:?}", dst_asn, dst_country, dst_city);
         self.processed = true;
+
         Ok(())
     }
 
-    fn finish(&mut self, ses: &mut Session) {}
+    fn finish(&mut self, ses: &mut Session) {
+        ses.add_field(&"srcIp", &json!(self.src_ip.addr));
+        ses.add_field(&"src.ASN", &json!(self.src_ip.asn));
+        ses.add_field(&"src.GEO", &json!(self.src_ip.country));
+        ses.add_field(&"src.GEOCity", &json!(self.src_ip.city));
+
+        ses.add_field(&"dstIp", &json!(self.dst_ip.addr));
+        ses.add_field(&"dst.ASN", &json!(self.src_ip.asn));
+        ses.add_field(&"dst.GEO", &json!(self.src_ip.country));
+        ses.add_field(&"dst.GEOCity", &json!(self.src_ip.city));
+    }
+}
+
+fn asn_to_string(asn: &Option<geoip2::Asn>) -> String {
+    match asn {
+        None => String::new(),
+        Some(asn) => {
+            match (
+                asn.autonomous_system_number,
+                asn.autonomous_system_organization,
+            ) {
+                (Some(num), Some(org)) => format!("{} {}", num, org),
+                (Some(num), None) => format!("{}", num),
+                (None, Some(org)) => format!("{}", org),
+                (None, None) => String::new(),
+            }
+        }
+    }
+}
+
+fn country_to_string(country: &Option<geoip2::Country>) -> String {
+    match country {
+        None => String::new(),
+        Some(country) => match &country.country {
+            None => String::new(),
+            Some(country) => country.iso_code.unwrap_or_default().to_string(),
+        },
+    }
+}
+
+fn city_to_string(city: &Option<geoip2::City>) -> String {
+    match city {
+        None => String::new(),
+        Some(city) => match &city.city {
+            None => String::new(),
+            Some(c) => match &c.names {
+                None => String::new(),
+                Some(names) => match names.get("en") {
+                    None => String::new(),
+                    Some(name) => name.to_string(),
+                },
+            },
+        },
+    }
 }
 
 #[no_mangle]
