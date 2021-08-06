eb225c5a88f3a3b770b7e733f92b413488e9e886
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-08-06 19:28:04 +0800

    add ICMP protocol dissect support

--- api/src/dissectors/network/icmp.rs
+++ api/src/dissectors/network/icmp.rs
@@ -1,13 +1,25 @@
 use super::{Error, Layer};
 
+use nom::number::streaming::{be_u16, be_u32, be_u8};
+use nom::IResult;
+
 #[derive(Default)]
 pub struct Dissector {}
 
 impl super::Dissector for Dissector {
     #[inline]
-    fn dissect(&self, _buf: &[u8], _offset: u16) -> Result<Option<Layer>, Error> {
-        return Err(Error::UnsupportProtocol(format!(
-            "Unsupport protocol: ICMP"
-        )));
+    fn dissect(&self, buf: &[u8], _offset: u16) -> Result<Option<Layer>, Error> {
+        dissect(buf).or(Err(Error::CorruptPacket(String::from(
+            "Corrupt ICMP packet",
+        ))))?;
+
+        return Ok(None);
     }
+}
+
+fn dissect(buf: &[u8]) -> IResult<&[u8], ()> {
+    let (buf, _) = be_u8(buf)?;
+    let (buf, _) = be_u16(buf)?;
+    let (buf, _) = be_u32(buf)?;
+    Ok((buf, ()))
 }
