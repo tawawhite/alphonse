0e9a856e1c6f356f27df3e43215b41dc19e542ba
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-08-12 20:55:08 +0800

    build script support cmake options

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-mkdir -p build && cd build && cmake .. && make -j $(nproc) && make install && cd ..
+mkdir -p build && cd build && cmake ${CMAKE_OPTIONS} .. && make -j $(nproc) && make install && cd ..
 
 export PKG_CONFIG_PATH=$(pwd)/build/install/lib/pkgconfig:$(pwd)/build/install/lib64/pkgconfig:$(pwd)/build/install/lib/x86_64-linux-gnu/pkgconfig
 export LD_LIBRARY_PATH=$(pwd)/build/install/lib:$(pwd)/build/install/lib64:$(pwd)/build/install/lib/x86_64-linux-gnu
