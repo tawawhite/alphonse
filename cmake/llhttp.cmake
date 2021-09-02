905a17d053538a26bd2863fe5413babc9853b0d5
Jack <15205739+JackLiar@users.noreply.github.com>
2021-09-02 19:03:10 +0800

    update llhttp to 6.0.5

--- cmake/llhttp.cmake
+++ cmake/llhttp.cmake
@@ -1,12 +1,12 @@
 if(OFFLINE_ENVIRONMENT)
-    set(llhttp_url ${CMAKE_CURRENT_SOURCE_DIR}/third_party/llhttp-6.0.2.tar.gz)
+    set(llhttp_url ${CMAKE_CURRENT_SOURCE_DIR}/third_party/llhttp-6.0.5.tar.gz)
 else()
-    set(llhttp_url https://github.com/JackLiar/llhttp-cmake/releases/download/v6.0.2/llhttp-6.0.2.tar.gz)
+    set(llhttp_url https://github.com/JackLiar/llhttp-cmake/releases/download/v6.0.5/llhttp-6.0.5.tar.gz)
 endif()
 
 ExternalProject_Add(llhttp
     URL ${llhttp_url}
-    URL_MD5 d781cce02e458c396ebf48412553e29e
+    URL_MD5 a53a6fb442a8514161c2b40813855764
     EXCLUDE_FROM_ALL ON
     PREFIX llhttp
     INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/install
