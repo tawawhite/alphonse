0a54567cdd35b083bdf61587f8c87138450021a5
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-09-21 08:55:20 +0800

    update boost to 1.77.0

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -1,12 +1,12 @@
 if(OFFLINE_ENVIRONMENT)
-    set(boost_url ${CMAKE_CURRENT_SOURCE_DIR}/third_party/boost_1_76_0.tar.bz2)
+    set(boost_url ${CMAKE_CURRENT_SOURCE_DIR}/third_party/boost_1_77_0.tar.bz2)
 else()
-    set(boost_url https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.bz2)
+    set(boost_url https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.bz2)
 endif()
 
 ExternalProject_Add(boost
     URL ${boost_url}
-    URL_MD5 33334dd7f862e8ac9fe1cc7c6584fb6d
+    URL_MD5 09dc857466718f27237144c6f2432d86
     EXCLUDE_FROM_ALL ON
     PREFIX boost
     CONFIGURE_COMMAND ""
