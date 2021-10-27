3b4e9e6f99a3a228336a0fd8577cc16b7c7202d5
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-10-27 19:46:00 +0800

    fix alphonse-arkime compile issue

--- arkime/src/lib.rs
+++ arkime/src/lib.rs
@@ -1,9 +1,11 @@
+#[cfg(feature = "fields")]
 #[macro_use]
 extern crate bitflags;
 
 pub mod config;
 #[cfg(feature = "fields")]
 pub mod field;
+#[cfg(feature = "stats")]
 pub mod stat;
 
 pub use config::Config;
